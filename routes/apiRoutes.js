4d6dba0329d0525dce3197fbbf58104231b77284
Emily Gaska <emilygaska@gmail.com>
2020-06-28 15:07:37 -0500

    Added API route for deleteing a note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,39 @@
 //Loads data from data files
-var noteData = require("../db/db.json");
+var notesData = require("../db/db.json");
 
 //Routing
 module.exports = function (app) {
-    app.get("/api/notes", function (req, res) {
-        res.json(noteData);
-    });
+  //GET method
+  app.get("/api/notes", function (req, res) {
+    res.json(noteData);
+  });
+  //POST method
+  app.post("/api/notes", function (req, res) {
+    if (notesData.length === 0) {
+      req.body.id = "0";
+    } else {
+      req.body.id = JSON.stringify(
+        JSON.parse(notesData[notesData.length - 1].id) + 1
+      );
+    }
+    console.log("req.body.id: " + req.body.id);
 
-    app.post("/api/notes", function (req, res) {
-        res.json(noteData)
-    });
+    notesData.push(req.body);
 
-    app.delete("/api/clear", function (req, res) {
-        // Empty out the arrays of data
-        noteData;
-        res.json({ ok: true });
-    });
-}
+    notesData = JSON.stringify(notesData);
+    fs.writeFileSync("./db/db.json", notesData);
+
+    res.json(req.body);
+  });
+
+  app.delete("/api/db/:id", function (req, res) {
+    let el = parseInt(req.params.id);
+    let tempNote = [];
+    for (let i = 0; i < notesData.length; i++) {
+      if (i !== el) {
+        tempNote.push(noteData[i]);
+      }
+    }
+    res.json("Note Deleted");
+  });
+};
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require("express");
+const express = require("express");
+const fs = require("fs");
+var path = require("path");
 
 //Tells node that we are creating an "express" server
 var app = express();
@@ -17,8 +19,22 @@ app.use(express.static("public"));
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+
+// app.post("/api/notes", function(req, res) {
+//   let notesData = fs.readFileSync("./db/db.json", "utf8");
+//   notesData = JSON.parse(notesData);
+//   let newNote= req.body;
+  
+//   notesData.push(req.body);
+//   notesData = JSON.stringify(notesData);
+//   fs.writeFile("./db/db.json", notesData, "utf8")
+
+//   console.log(notesData);
+// })
+
+
+
 //Listener to start the server
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-  });
-  
+  console.log("App listening on PORT: " + PORT);
+});
